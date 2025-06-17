f10626398299a70a70fcc79955dd5c7984e874ee
Manny <manny.blum@gmail.com>
2025-06-17 17:22:34 -0400

    removing teh color

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -38,7 +38,6 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-          style={{ backgroundColor: "red" }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
